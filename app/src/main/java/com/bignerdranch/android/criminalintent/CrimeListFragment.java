fb16c8da4ed7063c45bbe9cdfe1271201929476f
GarryMcKee <gmk0232@gmail.com>
2017-04-27 16:20:07 +0100

    add CrimeList Activity and Fragment, set CrimeList as launcher activity

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Garry on 27/04/2017.
+ */
+
+public class CrimeListFragment extends Fragment {
+}
