1d0a80f2f07fb438ee582f29bbbcd3315114a924
GarryMcKee <gmk0232@gmail.com>
2017-04-28 12:41:07 +0100

    Add requiresPolice boolean to Crime class.

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -13,6 +13,7 @@ public class Crime {
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
+    private boolean mRequiresPolice;
 
     public Crime() {
         mId = UUID.randomUUID();
@@ -45,5 +46,9 @@ public class Crime {
 
     public void setSolved(boolean solved) {
         mSolved = solved;
+    }
+
+    public boolean requiresPolice() {
+        return mRequiresPolice;
     }
 }
