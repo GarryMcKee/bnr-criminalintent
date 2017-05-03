2e39a5b3ad6ab91532461eb8f8b9e300d8dde613
GarryMcKee <gmk0232@gmail.com>
2017-05-03 16:02:04 +0100

    Add TimePickerFragment clas and layout

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/TimePickerFragment.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.DialogFragment;
+import android.widget.TimePicker;
+
+/**
+ * Created by Garry on 03/05/2017.
+ */
+
+public class TimePickerFragment extends DialogFragment {
+
+    private TimePicker mTimePicker;
+}
